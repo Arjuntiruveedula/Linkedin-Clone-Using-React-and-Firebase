8f97d590076ec70cced43363fec96757b7327a99
Apparao Tiruveedula <131840796+Arjuntiruveedula@users.noreply.github.com>
2024-05-31 11:45:26 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,9 @@ import ReactRefresh from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default {
   plugins: [ReactRefresh()],
+  build: {
+    chunkSizeWarningLimit: 1600, // Set your desired limit (in KiB)
+  },
 };
 
 
