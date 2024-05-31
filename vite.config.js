9a7ba01475074e0a20d39962d525bb3f3246974b
Apparao Tiruveedula <131840796+Arjuntiruveedula@users.noreply.github.com>
2024-05-31 11:55:57 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,9 +5,16 @@ import ReactRefresh from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default {
-  plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1600, // Set your desired limit (in KiB)
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+          }
+        },
+      },
+    },
   },
 };
 
